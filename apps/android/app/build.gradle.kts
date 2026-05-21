1fb23a8e582fbe9782cade2faff5337c044fe0fe
diasl <contact.dias@gmail.com>
2026-05-21 08:39:58 +0800

    feat(android): add core-splashscreen dependency

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
