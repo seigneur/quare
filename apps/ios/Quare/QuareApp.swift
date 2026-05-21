49e626bd64ef88ce6a358d2cd887063318c5a4b7
diasl <contact.dias@gmail.com>
2026-05-21 08:40:37 +0800

    feat(ios): display splash screen for 2 seconds before ContentView

--- apps/ios/Quare/QuareApp.swift
+++ apps/ios/Quare/QuareApp.swift
@@ -2,9 +2,22 @@ import SwiftUI
 
 @main
 struct QuareApp: App {
+    @State private var showSplash = true
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if showSplash {
+                SplashView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            withAnimation {
+                                showSplash = false
+                            }
+                        }
+                    }
+            } else {
+                ContentView()
+            }
         }
     }
 }
